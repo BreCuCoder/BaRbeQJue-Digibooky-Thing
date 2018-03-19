813ff9e4a8d8f9ae8696c91e67ae46e705c4eabc
BruCuCoder <brecht@superstudio.be>
2018-03-19 15:08:00 +0100

    removed the idea and iml files/dirs

---
+++ domain/src/main/java/com/barbeqjue/digibooky/domain/Author.java
@@ -0,0 +1,6 @@
+package com.barbeqjue.digibooky.domain;
+
+public class Author {
+    private String firstName;
+    private String lastName;
+}
---
+++ domain/src/main/java/com/barbeqjue/digibooky/domain/BookRepository.java
@@ -0,0 +1,21 @@
+package com.barbeqjue.digibooky.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BookRepository {
+    private Map<Integer, Book> books;
+    private static int bookIndex = 0;
+
+    public BookRepository() {
+
+        this.books = new HashMap<>();
+
+    }
+
+    public Book getBookByIsbn(int id){
+
+    }
+
+
+}
