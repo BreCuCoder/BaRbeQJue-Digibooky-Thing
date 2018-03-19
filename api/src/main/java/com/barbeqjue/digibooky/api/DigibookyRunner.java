1408b26a965dd2ee7b4abb4c049121cc2834f581
BruCuCoder <brecht@superstudio.be>
2018-03-19 11:54:33 +0100

    updated internal and external dependencies

--- api/src/main/java/com/barbeqjue/digibooky/api/DigibookyRunner.java
+++ api/src/main/java/com/barbeqjue/digibooky/api/DigibookyRunner.java
@@ -1,4 +1,21 @@
 package com.barbeqjue.digibooky.api;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan({"com.barbeqjue.digibooky.domain","com.barbeqjue.digibooky.services","com.barbeqjue.digibooky.api"})
+@SpringBootApplication
+
+
 public class DigibookyRunner {
+
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DigibookyRunner.class, args);
+
+    }
 }
