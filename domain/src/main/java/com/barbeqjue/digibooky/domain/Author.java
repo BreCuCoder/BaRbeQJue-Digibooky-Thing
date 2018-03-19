92e4325199c0f33f4709872efc3e88f3caf4acb9
BruCuCoder <brecht@superstudio.be>
2018-03-19 15:40:03 +0100

    first work on domain

--- domain/src/main/java/com/barbeqjue/digibooky/domain/Author.java
+++ domain/src/main/java/com/barbeqjue/digibooky/domain/Author.java
@@ -3,4 +3,13 @@ package com.barbeqjue.digibooky.domain;
 public class Author {
     private String firstName;
     private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
 }
--- domain/src/main/java/com/barbeqjue/digibooky/domain/BookRepository.java
+++ domain/src/main/java/com/barbeqjue/digibooky/domain/BookRepository.java
@@ -1,21 +1,62 @@
 package com.barbeqjue.digibooky.domain;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class BookRepository {
     private Map<Integer, Book> books;
     private static int bookIndex = 0;
 
     public BookRepository() {
-
         this.books = new HashMap<>();
-
     }
 
-    public Book getBookByIsbn(int id){
-    return null;
+    public Book getBookById(int id) {
+        return books.get(id);
     }
+
+    public List<Book> getBookByIsbn(String isbnRegex) {
+        List<Book> bookSearchResults = new ArrayList<>();
+        for (int id : books.keySet()) {
+            if (books.get(id).getIsbn().contains(isbnRegex)) {
+                bookSearchResults.add(books.get(id));
+            }
+        }
+        return Collections.unmodifiableList(bookSearchResults);
+    }
+
+    public List<Book> getBookByTitle(String titleRegex) {
+        List<Book> bookSearchResults = new ArrayList<>();
+        for (int id : books.keySet()) {
+            if (books.get(id).getTitle().contains(titleRegex)) {
+                bookSearchResults.add(books.get(id));
+            }
+        }
+        return Collections.unmodifiableList(bookSearchResults);
+    }
+
+    public List<Book> getBookByAuthor(String authorFirstNameRegex, String authorLastNameRegex) {
+        List<Book> bookSearchResults = new ArrayList<>();
+        for (int id : books.keySet()) {
+            if (books.get(id).getAuthor().getLastName().contains(authorLastNameRegex)) {
+                bookSearchResults.add(books.get(id));
+            }
+        }
+        return Collections.unmodifiableList(bookSearchResults);
+    }
+
+
+    public List<Book> getBookByAuthor(String authorLastNameRegex) {
+        List<Book> bookSearchResults = new ArrayList<>();
+        for (int id : books.keySet()) {
+            if (books.get(id).getAuthor().getLastName().contains(authorLastNameRegex)) {
+                bookSearchResults.add(books.get(id));
+            }
+        }
+        return Collections.unmodifiableList(bookSearchResults);
+    }
+
+
+
 
 
 }
