f2414ee5d8b7fdd76fadb0f6d54e898f9225f2dd
nielsdelestinne <nielsdelestinne@gmail.com>
2018-03-20 15:42:59 +0100

    Random book creation

--- gui/js/app.js
+++ gui/js/app.js
@@ -1,16 +1,82 @@
 /**
+ * ------------------
+ * --- TEST-DATA
+ * ------------------
+ */
+
+const ISBNS = ["978-1-891830-75-4",
+    "978-1-60309-050-6",
+    "978-1-60309-266-1",
+    "978-1-891830-71-6",
+    "978-1-60309-025-4",
+    "978-1-60309-047-6",
+    "978-1-60309-322-4",
+    "978-1-891830-85-3",
+    "978-1-60309-016-2",
+    "978-1-60309-265-4",
+    "978-1-60309-077-3",
+    "978-1-60309-369-9",
+    "978-1-60309-069-8",
+    "978-1-60309-042-1",
+    "978-1-60309-026-1"];
+
+const TITLES = [
+    "Writing bugs",
+    "How to become filthy rich",
+    "Dad, are we there yet?",
+    "How aliens catnapped my kid",
+    "How aliens kidnapped my cat",
+    "Thanks Obama",
+    "The reason behind reasoning",
+    "Dear brother and sister, a story by a mother who became a father.",
+    "How to BBQ without fire",
+    "My cat is trying to kill me",
+    "The story of a man killed by his cat",
+    "Why I killed my owner, a story by cat",
+    "Clouds are not real",
+    "How crashing my bike became a valuable life lesson",
+];
+
+const AUTHOR_FIRSTNAMES = [
+    "James",
+    "John",
+    "Jin",
+    "Jun",
+    "Jon",
+    "Jeremiah",
+    "Joseph",
+    "Jarred",
+    "Jerrad",
+    "Jamal",
+    "Jumro",
+    "Jeff",
+    "Jana",
+    "Josephina",
+    "Jaelle",
+];
+
+const AUTHOR_LASTNAMES = [
+    "Thomson",
+    "Tefferson",
+    "Tiko",
+    "Trump",
+    "Tron",
+    "Tremers",
+    "Tinkel",
+    "Tiange",
+    "Twinkel",
+    "Twain",
+    "Turrimo",
+    "Top",
+    "Tebone",
+    "Tutu",
+    "Taberlo",
+];
+
+/**
  * CONSTANTS
  */
 const BOOKS_RESOURCE = "http://localhost:9123/books";
-const BOOKDTO =
-    `{
-        "isbn": "161729520X", 
-        "title": "Spring in Action",
-        "author": {
-            "firstName": "Craig", 
-            "lastName": "Walls"
-        }
-    }`;
 
 /**
  * ------------------
@@ -39,11 +105,11 @@ function addARandomBook() {
         $.ajax({
             type: 'POST',
             url: BOOKS_RESOURCE,
-            data: BOOKDTO,
+            data: getRandomBookDto(),
             success: function () {
                 showBooks();
             },
-            error: function() {
+            error: function () {
                 alert("It didn't work!!");
             },
             contentType: "application/json",
@@ -57,6 +123,7 @@ function showBooks() {
     $.getJSON(BOOKS_RESOURCE, function (data) {
         console.log("--> Great success!");
         console.log(data);
+        imgCounter = 8500;
         if (data.length > 0) {
             $("#injectable").empty();
             data.forEach(function (item) {
@@ -77,16 +144,28 @@ function showBooks() {
         });
 }
 
+function getRandomBookDto() {
+    return `{
+        "isbn": "${ISBNS[Math.floor(Math.random() * 15)]}", 
+        "title": "${TITLES[Math.floor(Math.random() * 15)]}",
+        "author": {
+            "firstName": "${AUTHOR_FIRSTNAMES[Math.floor(Math.random() * 15)]}", 
+            "lastName": "${AUTHOR_LASTNAMES[Math.floor(Math.random() * 15)]}"
+        }
+    }`;
+}
+
 /**
  * ------------------
  * --- VIEW
  * ------------------
  */
+let imgCounter;
 const VIEW = {
     renderSingleItem: (function (book) {
         return `<div class="col-lg-3">
                     <div class="card">
-                      <img class="card-img-top img-fluid" src="https://picsum.photos/200" alt="${book.title}">
+                      <img class="card-img-top img-fluid" src="https://picsum.photos/150/150/?random${imgCounter++}" alt="${book.title}">
                       <div class="card-body">
                         <h4 class="card-title">${book.title}</h4>
                         <h5 class="card-subtitle mb-2 text-muted">${book.author.firstName} ${book.author.lastName}</h5>
